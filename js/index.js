a6e75bbe5730a0924c863ac3d20500f44aad8b19
Mona Lisa <thanhlong@Dragons-MacBook-Pro.local>
2023-07-22 20:07:38 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function () {
+    $('#slider-area').owlCarousel({
+        // items: 4,
+        // lazyLoad: true,
+        nav: true,
+        navText: [$('.button-prev'), $('.button-next')],
+        dots: false,
+        loop: true,
+        margin: 10,
+        items: 1,
+        margin: 10,
+        autoHeight: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        smartSpeed: 1000,
+        autoplayHoverPause: true
+
+    });
+
+
+
+});
+
